cbfd2b5a6709546fb5c08ef4869090d42ce4e170
Michael Edem Fiagbenu <edemykey@Michaels-MacBook-Pro.local>
2017-03-13 21:59:08 +0100

    First commit for chap 1

---
+++ semlavann/chap1.R
@@ -0,0 +1,9 @@
+# this tutorial was take from https://blogs.baylor.edu/rlatentvariable/sample-page/r-syntax/
+# 
+
+#Chapter 1: Introduction to R
+
+#Input data using c() function
+
+# create new dataset
+newData <- c(4,5,3,6,9)
